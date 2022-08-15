7eb999bb00ef5a70d7168641d58d674d1e730fd2
Immanuel Currah <immanueli.currah@gmail.com>
2022-08-15 13:51:16 -0400

    changing a spelling mistake

--- src/components/colorIndex/ColorIndex.jsx
+++ src/components/colorIndex/ColorIndex.jsx
@@ -2,7 +2,7 @@ export default function ColorIndex() {
   return (
     <div className="flex flex-row justify-center items-center m-4">
       <div className="h-2 w-2 bg-light-grey rounded-full"></div>
-      <p>companies information/</p>
+      <p>company's information/</p>
       <div className="h-2 w-2 bg-dark-grey rounded-full"></div>
       <p>perpay's total</p>
     </div>
